3cf508bf544e73939c27dda72bf5a00924d19e40
Naresh <naresh.khatri2345@gmail.com>
2022-01-20 01:48:00 +0530

    Add feature to add social links using socialSites.js, transitions and add opacity to inactive statuses

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -8,61 +8,16 @@
           :src="user.photoURL"
           style="border-radius: 50%; width: 200px; height: 200px"
         />
-        <q-btn class="q-ml-xl" label="change" @click="openDialog = true" />
+        <q-btn class="q-ml-xl" label="change" @click="editProfile" />
         <q-btn class="q-ml-xl" label="remove" />
       </q-card-section>
-      <div class="text-h4 text-bold">General</div>
-      <q-card-section>
-        <div class="row">
-          <q-input class="col q-mr-md" label="First name" v-model="firstName" />
-          <q-input class="col q-ml-md" label="Last name" v-model="lastName" />
-        </div>
-        <q-input label="Email" readonly v-model="user.email" />
-      </q-card-section>
-      <q-card-section class="flex justify-end">
-        <q-btn label="Save" />
-      </q-card-section>
-      <div class="text-h4 text-bold">Social accouts</div>
-      <q-card-section>
-        <q-icon size="xl" class="q-mr-md" style="color:#3b5998 " name="fab fa-facebook">
-          <q-badge rounded color="positive" floating label="✔" />
-        </q-icon>
-        <q-icon size="xl" class="q-mr-md" style="color:#8a3ab9 " name="fab fa-instagram">
-          <q-badge rounded color="positive" floating label="✔" />
-        </q-icon>
-        <q-icon size="xl" class="q-mr-md" style="color:#a5a200 " name="fab fa-snapchat">
-          <q-badge rounded color="positive" floating label="✔" />
-        </q-icon>
-        <q-icon size="xl" class="q-mr-md" style="color:#000000 " name="fab fa-github">
-          <q-badge rounded color="positive" floating label="✔" />
-        </q-icon>
-        <q-icon size="xl" class="q-mr-md" style="color:#229ED9 " name="fab fa-telegram">
-          <q-badge rounded color="negative" floating label="❌" />
-        </q-icon>
-        <q-icon size="xl" class="q-mr-md" style="color:#00acee  " name="fab fa-twitter">
-          <q-badge rounded color="positive" floating label="✔" />
-        </q-icon>
-      </q-card-section>
 
-      <div class="row" style="margin-top: 100px">
-        <div class="col-3">
-          <div>
-            <q-img class="shadow-10" :src="user.photoURL" style="border-radius: 50%; width: 100%" />
-          </div>
-        </div>
-        <div class="col-7">
-          <div class="text-white q-pa-md">
-            <div class="text-black text-bold text-h5">{{ user.name }}</div>
-            <div class="text-grey-7 text-subtitle">{{ user.email }}</div>
-          </div>
-        </div>
-        <div class="col-1 align-center"></div>
-      </div>
-      <div class="row q-mt-xl">
-        <div class="col-12">
-          <div class="text-grey-7 text-subtitle">My status</div>
-          <q-scroll-area style="height: 60px; max-width: 90vw; white-space: nowrap">
-            <span @click="setActiveStatus(status.id)" v-for="(status, i) in statuses" :key="i">
+      <div class="q-mt-xl text-h4 text-bold">Personal Stuff</div>
+      <q-card-section>
+        <div class="text-grey-7 text-subtitle">My status</div>
+        <q-scroll-area style="height: 60px; max-width: 90vw; white-space: nowrap">
+          <span @click="setActiveStatus(status.id)" v-for="(status, i) in statuses" :key="i">
+            <transition appear enter-active-class="animated bounceIn">
               <q-chip
                 v-if="status.id == user.status"
                 :label="status.text"
@@ -73,29 +28,22 @@
                 class="q-mr-sm"
                 :class="user.status == status.id ? 'shadow-10' : 'disabled'"
               />
-            </span>
-            <span @click="setActiveStatus(status.id)" v-for="(status, i) in statuses" :key="i">
-              <q-chip
-                v-if="status.id !== user.status"
-                :label="status.text"
-                :color="status.color"
-                :text-color="status.textColor"
-                size="17px"
-                :style="{ background: status.color, color: status.textColor }"
-                class="q-mr-sm"
-                :class="user.status == status.id ? 'shadow-10' : 'disabled'"
-              />
-            </span>
-          </q-scroll-area>
-        </div>
-      </div>
-      <div class="q-mt-xl"></div>
-      <div class="q-mt-xl">
-        <div class="text-grey-7 text-subtitle">
-          My interests
-          <q-btn flat color="black" icon="edit" @click="editInterests" />
-        </div>
-        <q-separator class="q-ma-md" />
+            </transition>
+          </span>
+          <span @click="setActiveStatus(status.id)" v-for="(status, i) in statuses" :key="i">
+            <q-chip
+              v-if="status.id !== user.status"
+              :label="status.text"
+              :color="status.color"
+              :text-color="status.textColor"
+              size="17px"
+              class="q-mr-sm shadow-10"
+              style="opacity: 0.6; cursor: pointer;"
+              :style="`background: ${status.color}; color: ${status.textColor}`"
+            />
+          </span>
+        </q-scroll-area>
+        <div class="text-grey-7 text-subtitle">My interests</div>
         <div class="q-gutter-xs text-black">
           <q-chip
             v-for="(interest, i) in getRandomInterests"
@@ -108,109 +56,171 @@
             class="q-mr-sm shadow-10"
           />
         </div>
-      </div>
+      </q-card-section>
+      <div class="q-mt-xl text-h4 text-bold">Social accouts</div>
+      <q-card-section>
+        <q-icon
+          v-for="(site, i) in socialSites"
+          :key="i"
+          style="opacity: 1;"
+          :style="`color: ${site.color};opacity: ${editSocialSiteName &&
+          editSocialSiteName == site.name ? 1 : 0.5}`"
+          :name="site.icon"
+          size="xl"
+          class="q-mr-sm q-pa-sm bg-prima ry"
+          @click="editSocialSiteName = site.name"
+        >
+          <q-badge rounded color="positive" floating label="✔" />
+        </q-icon>
+        <span class="text-subtitle" v-if="!editSocialSiteName">click to add</span>
+        <transition
+          appear
+          enter-active-class="animated zoomInDown"
+          enter-leave-class="animated zoomOutDown"
+        >
+          <q-input
+            v-if="editSocialSiteName"
+            class="q-ma-md"
+            v-model="text"
+            :label="`Add ${editSocialSiteName} profile URL`"
+          >
+            <template v-slot:append>
+              <q-icon
+                v-if="text !== ''"
+                name="close"
+                @click="editSocialSiteName = ''"
+                class="cursor-pointer"
+              />
+            </template>
+          </q-input>
+        </transition>
+      </q-card-section>
+      <div class="q-mt-xl text-h4 text-bold">General</div>
+      <q-card-section style="margin-bottom:400px">
+        <div class="row">
+          <q-input class="col q-mr-md q-my-sm" label="Full name" v-model="userName" />
+          <!-- <q-input class="col q-ml-md q-my-sm" label="Last name" v-model="lastName" /> -->
+        </div>
+        <q-input label="Email" class="q-my-sm" readonly v-model="user.email" />
+        <q-btn style="float: right" class="q-my-sm" label="Save" />
+      </q-card-section>
     </div>
   </q-page>
 </template>
 
-<script>
+<script setup>
 import axios from "axios";
 import { api } from "boot/axios";
 
 
+import ProfileEdit from "../components/ProfileEdit.vue";
+
 import statusesJSON from "../data/profileStatuses";
 import interestsJSON from "../data/profileInterests";
+import socialSites from "../data/socialSites";
+
 import { ref, computed, onMounted } from "vue";
 import { useStore } from "vuex";
 import { useQuasar } from "quasar";
 
-export default {
-  components: {},
-  setup() {
-    const store = useStore();
-    const $q = useQuasar();
+const store = useStore();
+const $q = useQuasar();
 
-    const statuses = ref(statusesJSON);
-    const interests = ref(interestsJSON);
-    const activeStatus = ref("online");
-    // const badges = ref([
-    //   require("../assets/badges/new.svg"),
-    //   require("../assets/badges/best.svg"),
-    //   require("../assets/badges/diamond.svg"),
-    //   require("../assets/badges/fast.svg"),
-    //   require("../assets/badges/heart.svg"),
-    //   require("../assets/badges/mostLiked.svg"),
-    //   require("../assets/badges/verified.svg"),
-    // ]);
+const statuses = ref(statusesJSON);
+const interests = ref(interestsJSON);
+const activeStatus = ref("online");
 
-    // const statuses = store.state.profileStatuses;
-    // const interests = store.state.profileInterests;
 
-    const user = computed(() => store.state.userData);
-    // const photoURL = computed(()=> store.state.userData.photoURL);
-    // const photoURL = ref("");
-    // photoURL.value = "http://localhost:4000/user/getPhotoURL/ipZVQgDR5Wdixl5EfR48I6rPtkG3"
-    // photoURL.value = computed(() =>
-    // store.state.userData.customProfilePic
-    //   ? "http://localhost:4000/user/getPhotoURL/ipZVQgDR5Wdixl5EfR48I6rPtkG3"
-    //   : store.state.userData.photoURL
+const editSocialSiteName = ref('');
+
+// const badges = ref([
+//   require("../assets/badges/new.svg"),
+//   require("../assets/badges/best.svg"),
+//   require("../assets/badges/diamond.svg"),
+//   require("../assets/badges/fast.svg"),
+//   require("../assets/badges/heart.svg"),
+//   require("../assets/badges/mostLiked.svg"),
+//   require("../assets/badges/verified.svg"),
+// ]);
+
+// const statuses = store.state.profileStatuses;
+// const interests = store.state.profileInterests;
+
+const userName = ref('John');
+const user = computed(() => store.state.userData);
+setTimeout(() => {
+  userName.value = user.value.name
+}, 2000);
+// const photoURL = computed(()=> store.state.userData.photoURL);
+// const photoURL = ref("");
+// photoURL.value = "http://localhost:4000/user/getPhotoURL/ipZVQgDR5Wdixl5EfR48I6rPtkG3"
+// photoURL.value = computed(() =>
+// store.state.userData.customProfilePic
+//   ? "http://localhost:4000/user/getPhotoURL/ipZVQgDR5Wdixl5EfR48I6rPtkG3"
+//   : store.state.userData.photoURL
+// );
+
+const getRandomInterests = computed(() => {
+  const interestsCopy = interests;
+  return interestsCopy.value.sort(() => 0.5 - Math.random()).slice(0, 4);
+});
+
+onMounted(async () => {
+  // setInterval(() => {
+  // photoURL.value += "?";
+  // console.log("updating profile pic", photoURL.value);
+  // }, 1000);
+  try {
+    setTimeout(() => {
+    }, 0);
+    // const res = await axios.get(
+    //   "https://jntua.plasmatch.in/singleResultv2/19fh1a0546/R19/B.TECH/II/II"
     // );
-
-    const getRandomInterests = computed(() => {
-      const interestsCopy = interests;
-      return interestsCopy.value.sort(() => 0.5 - Math.random()).slice(0, 4);
+  } catch (e) {
+    console.log(e);
+  }
+});
+const editProfile = () => {
+  $q.dialog({
+    component: ProfileEdit,
+  })
+    .onOk(() => {
+      // console.log('OK')
+    })
+    .onCancel(() => {
+      // console.log('Cancel')
+    })
+    .onDismiss(() => {
+      // console.log('I am triggered on both OK and Cancel')
     });
-
-    onMounted(async () => {
-      // setInterval(() => {
-      // photoURL.value += "?";
-      // console.log("updating profile pic", photoURL.value);
-      // }, 1000);
-      try {
-        setTimeout(() => {
-        }, 0);
-        // const res = await axios.get(
-        //   "https://jntua.plasmatch.in/singleResultv2/19fh1a0546/R19/B.TECH/II/II"
-        // );
-      } catch (e) {
-        console.log(e);
-      }
+  // $q.dialog({
+  //   title: "Edit Profile",
+  //   component: "edit-profile",
+  //   width: "500px",
+  // });
+};
+const setActiveStatus = (id) => {
+  console.log("setting");
+  activeStatus.value = id;
+  store.commit("setUserStatus", id);
+  api
+    .post("/user/changeStatus", {
+      status: id,
+      uid: user.value.uid,
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
     });
-
-    const setActiveStatus = (id) => {
-      console.log("setting");
-      activeStatus.value = id;
-      store.commit("setUserStatus", id);
-      api
-        .post("/user/changeStatus", {
-          status: id,
-          uid: user.value.uid,
-        })
-        .then(function (response) {
-          console.log(response);
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-    };
-    const editInterests = () => {
-      $q.dialog({
-        title: "Edit Interests",
-        message: "This feature is not implemented yet 😢",
-        width: "500px",
-      });
-    };
-    return {
-      user,
-      // photoURL,
-      statuses,
-      interests,
-      activeStatus,
-      getRandomInterests,
-      setActiveStatus,
-      editInterests,
-    };
-  },
+};
+const editInterests = () => {
+  $q.dialog({
+    title: "Edit Interests",
+    message: "This feature is not implemented yet 😢",
+    width: "500px",
+  });
 };
 </script>
 
